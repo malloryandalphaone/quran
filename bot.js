c49ac98282137916ce61b186b87e1c6174967d8f
malloryandalphaone <42677997+malloryandalphaone@users.noreply.github.com>
2019-03-01 16:30:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,7 @@ const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
 const prefix = '!';
+var x = client.channels.get("548291473179803659");
 
 client.on('message', message => {
 	if(message.content.startsWith(prefix + 'quran')) {
